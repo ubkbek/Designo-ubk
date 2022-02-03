cfa5be7c3ec91160932918cbce27c9fb6227b164
ubkbek <ulugbekjovliyev01001@gmail.com>
2022-02-03 17:21:18 +0500

    change footer

--- js/main.js
+++ js/main.js
@@ -2,7 +2,6 @@ var elHeader = document.querySelector(".header")
 var elBtn = document.querySelector(".header__btn")
 
 
-
 elBtn.addEventListener("click", () => {
     elHeader.classList.toggle("header--active")
 })
