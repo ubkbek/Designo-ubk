c5d2e1eb63fdb80a5f90cff89deba1ad700464da
ubkbek <ulugbekjovliyev01001@gmail.com>
2022-02-03 16:13:55 +0500

    add menu for mobile devisec

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var elHeader = document.querySelector(".header")
+var elBtn = document.querySelector(".header__btn")
+
+
+
+elBtn.addEventListener("click", () => {
+    elHeader.classList.toggle("header--active")
+})
